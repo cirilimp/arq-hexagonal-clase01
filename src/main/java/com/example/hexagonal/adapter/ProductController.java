948b5d5df3af1b699162c1b316d0fdc460b9777a
jmmoliner <jmmoliner@minsait.com>
2024-07-09 10:11:11 +0200

    clase01

---
+++ src/main/java/com/example/hexagonal/adapter/ProductController.java
@@ -0,0 +1,25 @@
+package com.example.hexagonal.adapter;
+import com.example.hexagonal.application.ProductService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+
+@Path("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Inject
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getProducts() {
+        return productService.getProducts();
+    }
+}
